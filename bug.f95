738f2d25e64d3cdca3d5bd93c47314686003e3c6
Jodie Burchell <jodieburchell@Jodies-MacBook-Pro.local>
2016-09-08 19:02:52 +1000

    Mostly finished first module in tutorial

---
+++ bug.f95
@@ -0,0 +1,10 @@
+program bug
+!this program is full of errors
+real :: a,b,c
+print *, 'Enter a number'
+read *,b
+print *, 'Enter another number'
+read *,c
+a = b + c
+print *,a
+end program bug
---
+++ io.f95
@@ -0,0 +1,6 @@
+program io
+real :: x,y,z
+print *, 'Enter the values x, y and z'
+read *, x,y,z
+print *, 'The values you types for z, y, x are: ', z,y,x
+end program io
